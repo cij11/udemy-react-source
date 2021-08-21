a913646874e739004ab782d93b73183008041c90
Christopher Jolly <chris.jolly@bachcare.co.nz>
2021-08-21 16:23:24 +1200

    wip

--- section-18/blog/src/reducers/index.js
+++ section-18/blog/src/reducers/index.js
@@ -1,5 +1,6 @@
-import { combineReducers } from 'redux';
+import { combineReducers } from 'redux'
+import postsReducer from './posts'
 
 export default combineReducers({
-  dummy: () => 999
-});
+    posts: postsReducer,
+})
---
+++ section-18/blog/src/reducers/posts.js
@@ -0,0 +1,10 @@
+// Rules of reducers
+// Must return ANY value besides 'undefined' (even null is acceptable)
+// Produces 'state', or data to be used inside of your app,
+// using only previous state and the action (reducers are pure)
+// Must not return reach 'out of itself' to decide what value to return
+// Must not mutate its input 'state' argument
+
+// Reducer arguments: (state = initialState, action)
+
+export default () => {}
