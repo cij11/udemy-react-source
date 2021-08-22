a50135e448c0b7385d209fd62a7fd7a58d6e1bee
Christopher Jolly <chris.jolly@bachcare.co.nz>
2021-08-22 18:43:37 +1200

    Scaffolding routes

--- streamy/client/src/components/App.js
+++ streamy/client/src/components/App.js
@@ -1,23 +1,10 @@
 import React from 'react'
-import { BrowserRouter, Route, Link } from 'react-router-dom'
-
-const PageOne = () => {
-    return (
-        <div>
-            PageOne
-            <Link to="/pagetwo">Navigate to page 2</Link>
-        </div>
-    )
-}
-
-const PageTwo = () => {
-    return (
-        <div>
-            PageTwo
-            <button>Click me</button>
-        </div>
-    )
-}
+import { BrowserRouter, Route } from 'react-router-dom'
+import StreamCreate from './streams/StreamCreate'
+import StreamDelete from './streams/StreamDelete'
+import StreamEdit from './streams/StreamEdit'
+import StreamList from './streams/StreamList'
+import StreamShow from './streams/StreamShow'
 
 const App = () => {
     // Routing rule for none exact: pathFromUrl.contains(path)
@@ -25,8 +12,12 @@ const App = () => {
     return (
         <div>
             <BrowserRouter>
-                <Route path="/" exact component={PageOne} />
-                <Route path="/pagetwo" exact component={PageTwo} />
+                <Route path="/" exact component={StreamList} />
+                <Route path="/streams/new" exact component={StreamCreate} />
+                <Route path="/streams/edit" exact component={StreamEdit} />
+                <Route path="/streams/create" exact component={StreamCreate} />
+                <Route path="/streams/delete" exact component={StreamDelete} />
+                <Route path="/streams/show" exact component={StreamShow} />
             </BrowserRouter>
         </div>
     )
