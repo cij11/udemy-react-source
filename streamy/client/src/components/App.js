04ab1028aea01e418268b480fa6128c989b52624
Christopher Jolly <chris.jolly@bachcare.co.nz>
2021-08-22 21:02:49 +1200

    Add 'visible everywhere' header

--- streamy/client/src/components/App.js
+++ streamy/client/src/components/App.js
@@ -5,13 +5,15 @@ import StreamDelete from './streams/StreamDelete'
 import StreamEdit from './streams/StreamEdit'
 import StreamList from './streams/StreamList'
 import StreamShow from './streams/StreamShow'
+import Header from './header'
 
 const App = () => {
     // Routing rule for none exact: pathFromUrl.contains(path)
     // Routing rule for exact: pathFromUrl == path
     return (
-        <div>
+        <div className="ui container">
             <BrowserRouter>
+                <Header />
                 <Route path="/" exact component={StreamList} />
                 <Route path="/streams/new" exact component={StreamCreate} />
                 <Route path="/streams/edit" exact component={StreamEdit} />
