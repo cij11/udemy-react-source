7d25018c950ecbfd4a3838b458f39764f9efaa05
Christopher Jolly <chris.jolly@bachcare.co.nz>
2021-09-26 21:33:49 +1300

    Google sign in/out button working

--- streamy/client/src/components/GoogleAuth.js
+++ streamy/client/src/components/GoogleAuth.js
@@ -35,10 +35,33 @@ class GoogleAuth extends React.Component {
 
     renderAuthButton() {
         if (this.state.isSignedIn == null) {
-            return <div>I don't know if we are signed in</div>
+            return null
         } else if (this.state.isSignedIn) {
-            return <div>I am signed in</div>
-        } else return <div>I am not signed in</div>
+            return (
+                <button
+                    onClick={this.onSignOut}
+                    className="ui red google button"
+                >
+                    <i className="google icon" /> Sign Out
+                </button>
+            )
+        } else
+            return (
+                <button
+                    onClick={this.onSignIn}
+                    className="ui red google button"
+                >
+                    <i className="google icon" /> Sign in with Google
+                </button>
+            )
+    }
+
+    onSignIn = () => {
+        this.auth.signIn()
+    }
+
+    onSignOut = () => {
+        this.auth.signOut()
     }
 }
 
