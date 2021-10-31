13cb176bdf7e55ea3a9e0b5f5822ce58ba22ac6b
Christopher Jolly <chris.jolly@bachcare.co.nz>
2021-10-31 22:47:22 +1300

    Implementing patch edit

--- streamy/client/src/components/streams/StreamEdit.js
+++ streamy/client/src/components/streams/StreamEdit.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import { connect } from 'react-redux'
-import { fetchStream } from '../../actions'
+import { fetchStream, editStream } from '../../actions'
+import StreamForm from './StreamForm'
+import { pick } from 'lodash'
 
 class StreamEdit extends React.Component {
     componentDidMount() {
@@ -8,12 +10,29 @@ class StreamEdit extends React.Component {
         this.props.fetchStream(this.props.match.params.id)
     }
 
+    handleSubmit = (formValues) => {
+        console.log(formValues)
+        this.props.editStream(this.props.match.params.id, formValues)
+    }
+
     render() {
         if (!this.props.stream) {
             return <div>Loading</div>
         }
 
-        return <div>{this.props.stream.title}</div>
+        return (
+            <div>
+                <h3>Edit a Stream</h3>
+                <StreamForm
+                    onSubmit={this.handleSubmit}
+                    initialValues={pick(
+                        this.props.stream,
+                        'title',
+                        'description'
+                    )}
+                />
+            </div>
+        )
     }
 }
 
@@ -25,6 +44,7 @@ const mapStateToProps = (state, ownProps) => {
 
 const mapDispatchToProps = {
     fetchStream,
+    editStream,
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(StreamEdit)
