b2c54150862e7f3b52ea2871e4fdeef1274c91ee
Christopher Jolly <chris.jolly@bachcare.co.nz>
2021-11-03 21:29:34 +1300

    Stream Show skeleton

--- streamy/client/src/components/App.js
+++ streamy/client/src/components/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Router, Route } from 'react-router-dom'
+import { Router, Route, Switch } from 'react-router-dom'
 import StreamCreate from './streams/StreamCreate'
 import StreamDelete from './streams/StreamDelete'
 import StreamEdit from './streams/StreamEdit'
@@ -15,15 +15,24 @@ const App = () => {
         <div className="ui container">
             <Router history={history}>
                 <Header />
-                <Route path="/" exact component={StreamList} />
-                <Route path="/streams/new" exact component={StreamCreate} />
-                <Route path="/streams/edit/:id" exact component={StreamEdit} />
-                <Route
-                    path="/streams/delete/:id"
-                    exact
-                    component={StreamDelete}
-                />
-                <Route path="/streams/show" exact component={StreamShow} />
+                <Switch>
+                    {
+                        // Switch makes only one Route show. i.e, streams/:id and streams/new could both match on 'new'. With switch, only match on the first one. Having said that, this was working for me even before including Switch
+                    }
+                    <Route path="/" exact component={StreamList} />
+                    <Route path="/streams/new" exact component={StreamCreate} />
+                    <Route
+                        path="/streams/edit/:id"
+                        exact
+                        component={StreamEdit}
+                    />
+                    <Route
+                        path="/streams/delete/:id"
+                        exact
+                        component={StreamDelete}
+                    />
+                    <Route path="/streams/:id" exact component={StreamShow} />
+                </Switch>
             </Router>
         </div>
     )
